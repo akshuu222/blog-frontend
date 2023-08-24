c9de805071cfc981369bb43305b6093b14efc350
askhuu222 <lmakshath123@gmail.com>
2023-08-24 12:20:26 +0530

    first commit

--- src/Slices/apiSlices.js
+++ src/Slices/apiSlices.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.URL,
+    baseUrl: "https://blog-app-api-vie7.onrender.com/",
     credentials: "include",
   }),
   endpoints: (builder) => ({}),
